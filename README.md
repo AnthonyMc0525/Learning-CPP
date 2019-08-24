86405e76734eee260a71a92d8c1de858eb762a2a
Anthony McMellen <anthonymc456@gmail.com>
2019-08-23 22:18:10 -0400

    Created initial structure for repository
    - Added a README for basic instructions on how to run the code
    - Added the learning-exercises folder to put all my code in
    - Added a note folder to put all of my notes in

---
+++ README.md
@@ -0,0 +1,6 @@
+# This Repository is for the use of any one.
+
+you need a compiler to be able to run c++ code
+I am on mac, so I use x-code 
+  the command to compile c++ code in the terminal is g++
+  you will then get a file called a.out. to run this just type ./a.out
---
+++ learning-exercises/add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  int sum;
+
+  cout << "Enter your first number: " << endl;
+  cin >> a;
+
+  cout << "Enter your second number: " << endl;
+  cin >> b;
+
+  sum = a + b;
+  cout << "The sum is " << sum;
+
+  return 0;
+}
